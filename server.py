8fefefe59ed04189649f53fa1521fff32c42d69f
Roger Howard <rogerhoward@gmail.com>
2016-05-19 11:10:43 -0700

    Add port

--- server.py
+++ server.py
@@ -21,4 +21,4 @@ for loader, mod_name, ispkg in modules:
                 app.register_blueprint(obj)
 
 
-app.run(debug=config.debug, host=config.host)
+app.run(debug=config.debug, host=config.host, port=config.port)
